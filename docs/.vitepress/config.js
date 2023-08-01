7e1317e22617e4de9a09781be0dbf028bd6efca0
lassev05 <vestergaardlasse2@gmail.com>
2023-08-01 22:41:27 +0200

    Fix plugins bug

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -7,24 +7,38 @@ export default defineConfig({
     [
       "script",
       {
-        src: "https://unpkg.com/@badcss/core@0.1.3/lib",
+        src: "https://unpkg.com/@badcss/core@0.1.4/lib",
         defer: true,
         type: "module",
       },
     ],
   ],
+
   ignoreDeadLinks: true,
   title: "The Most Bad CSS framework",
   description: "",
   themeConfig: {
+    search: {
+      provider: "local",
+    },
     nav: [
       { text: "Documention", link: "/docs" },
+      {
+        text: "current",
+        items: [
+          {
+            text: "0.1.0 >",
+            link: "/docs",
+          },
+        ],
+      },
       { text: "Our Team", link: "/team" },
     ],
 
     sidebar: [
       {
         text: "Getting Started",
+        collapsed: false,
         items: [
           { text: "Installation", link: "/docs" },
           { text: "Components", link: "/components" },
@@ -35,6 +49,7 @@ export default defineConfig({
 
       {
         text: "Components",
+        collapsed: false,
         items: [{ text: "Button", link: "/components/button" }],
       },
     ],
