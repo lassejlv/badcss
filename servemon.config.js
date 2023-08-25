60581a6da1a281edb09decce50c0241a3d911b89
lassev05 <vestergaardlasse2@gmail.com>
2023-08-25 10:01:03 +0200

    Fixed #14

---
+++ servemon.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  projectName: "my-project",
+  port: 3001, // Port to use
+  directory: "./", // Directory to use
+  pkgManager: "servemon", // Package manager you want to use
+  watch: true, // Watch for changes
+  open: true, // Open the browser
+  logger: true, // Enable logger
+};
